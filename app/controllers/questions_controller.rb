094dcfcf4d334b7925eaa9fbfdf5903c90f8ee48
balashovmb <balashovmb@gmail.com>
2017-02-20 16:52:56 +0500

    All actions in question controller maded

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:show, :edit]
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -16,25 +16,39 @@ class QuestionsController < ApplicationController
   def edit
   end
 
-<<<<<<< HEAD
+
   def create
     @question = Question.create(question_params)
-    redirect_to @question
+    
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
-=======
->>>>>>> 8373d7c1eb47306bd12a43fe3f395b1dda9bbb27
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+  end
+
   private
 
   def load_question
     @question = Question.find(params[:id])    
   end
 
-<<<<<<< HEAD
+
   def question_params
     params.require(:question).permit(:title, :body)
   end
 
-=======
->>>>>>> 8373d7c1eb47306bd12a43fe3f395b1dda9bbb27
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -55,13 +55,67 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves the new question in the database'  do
-        expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+        expect { post :create, params:{ question: attributes_for(:question)} }.to change(Question, :count).by(1)
       end
       it 'rediredts to show view' do
-        post :create, question: attributes_for(:question)
+        post :create, params:{ question: attributes_for(:question)} 
         expect(response).to redirect_to question_path(assigns(:question))
       end
     end  
+    
+    context 'with invalid attributes' do  
+      it 'does not save the question' do
+        expect { post :create, params:{ question: attributes_for( :invalid_question)} }.to_not change(Question, :count)        
+      end
+      
+      it 're-renders new view' do
+        post :create, params:{ question: attributes_for(:invalid_question)}
+        expect(response).to render_template :new
+      end
+    end
   end
 
+  describe 'PATCH #update' do
+    context 'valid attributes' do
+      it 'assigns the requested question to @question' do
+        patch :update, params:{ id: question, question: attributes_for(:question)}
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'changes question attributes' do
+        patch :update, params: {  id: question, question: {title: 'new title', body: 'new body'}}
+        question.reload
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new body'       
+      end
+
+      it 'rediredts to the updated question' do
+        patch :update, params:{ id: question, question: attributes_for(:question) }   
+        expect(response).to redirect_to question     
+      end
+    end
+    context 'invalid attributes' do
+      before { patch :update, params: { id: question, question: {title: 'new title', body: nil} }}
+      it 'does not change attributes' do
+        question.reload
+        expect(question.title).to eq 'MyString'
+        expect(question.body).to eq 'MyText'                 
+      end
+      it 're-renders edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before { question }
+    it 'delete question' do
+      expect { delete :destroy, params:{ id: question} }.to change(Question, :count ).by(-1)
+    end
+
+    it 'redirect_to index view' do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to questions_path 
+    end
+  end
 end
