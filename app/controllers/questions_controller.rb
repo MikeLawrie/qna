d8e85a42c73a52e022d25b6b91f8fbc60b39a07b
balashovmb <balashovmb@gmail.com>
2017-02-20 16:52:56 +0500

    All actions in question controller maded

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,5 @@
 class QuestionsController < ApplicationController
-
-  before_action :load_question, only: [:show, :edit]
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -19,17 +18,31 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.create(question_params)
-    redirect_to @question
+    
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
 
   def load_question
     @question = Question.find(params[:id])    
-  end
-
-  def index
-    @questions = Question.all
   end
 
   def question_params
