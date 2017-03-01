195260cea9a9764e3539d72525c8100181b8dbd5
balashovmb <balashovmb@gmail.com>
2017-03-01 20:54:37 +0500

    Lesson5 answers controller create action spec in process

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,28 @@
 class AnswersController < ApplicationController
-  before_action :set_question, only: [:new]
+  before_action :set_question, only: [:new, :que]
 
   def new
     @answer = Answer.new
-  end  
+  end
+
+  def create
+    @answer = Answer.create(answer_params)
+    
+    if @answer.save
+      redirect_to @answer, notice: "Your answer successfully created."   
+    else
+      render :new
+    end
+  end    
 
   private 
 
   def set_question
     @question = Question.find(params[:question_id])    
   end
+
+  def answer_params
+    params.require(:answer).permit(:question, :body)
+  end  
 
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -21,6 +21,32 @@ RSpec.describe AnswersController, type: :controller do
     it 'renders new view' do
       expect(response).to render_template :new
     end
-  end  
+  end 
+
+  describe 'POST #create' do
+    sign_in_user
+
+    context 'with valid attributes' do
+      it 'saves the new answer in the database'  do
+        expect { post :create, params:{ answer: attributes_for(:answer), question_id: question} }.to change(Answer, :count).by(1)
+      end
+      it 'redirects to show view' do
+        post :create, params:{ answer: attributes_for(:answer)} 
+        expect(response).to redirect_to question_path(assigns(:answer))
+      end
+    end  
+    
+    context 'with invalid attributes' do  
+      it 'does not save the question' do
+        expect { post :create, params:{ answer: attributes_for( :invalid_answer)} }.to_not change(Answer, :count)        
+      end
+      
+      it 're-renders new view' do
+        post :create, params:{ answer: attributes_for(:invalid_answer)}
+        expect(response).to render_template :new
+      end
+    end
+  end
+
 
 end
