385c2ebe988ad4e17494c7883c2245426a69ce72
balashovmb <balashovmb@gmail.com>
2017-03-01 12:48:06 +0500

    Lesson5. New in answers controllers and create answer specs updated

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
  
+  let(:question) { create(:question)}
 
   describe 'GET #new' do
-  let(:question) { create(:question)}
 
     sign_in_user
 
-
     before { get :new, params:{ question_id: question}} 
+
+    it 'assigns the question selected to give an answer to @question' do
+      expect(assigns[:question]).to eq question
+    end
 
     it 'assigns a new Answer to @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
